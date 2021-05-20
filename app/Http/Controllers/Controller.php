d536a791809ed359a1d513f142fd235af1cdca43
ipiedrahiv <ipiedrahiv@eafit.edu.co>
2021-05-19 20:05:38 -0500

    controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,21 +7,36 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
 
-    public static $quotes = array("The Black Knight Always Triumphs! - Monty
-Python",
-    "Anyone who has never made a mistake has never tried anything new -
-Albert Einstein",
-    "Never Stop Exploring - The North Face",
-    "Be yourself; everyone else is already taken - Oscar Wilde",
-    "So many books, so little time - Frank Zappa",
-    "Be the change that you wish to see in the world - Mahatma Gandhi",
+    public static $quotes = array("https://imagestallerdos.s3.amazonaws.com/quote_1.png",
+    "https://imagestallerdos.s3.amazonaws.com/quote_2.jpeg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_3.jpeg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_4.jpeg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_5.jpeg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_6.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_7.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_8.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_9.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_10.jpeg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_11.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_12.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_13.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_14.jpg",
+    "https://imagestallerdos.s3.amazonaws.com/quote_15.jpg",
     );
 
     public function index()
     {
+        $data = [];
+
         $totalQuotes = (count(Controller::$quotes));
         $randomNumber = (rand(0,($totalQuotes-1)));
         $randomQuote = Controller::$quotes[$randomNumber];
-        return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
+
+        $data['image'] = $randomQuote;
+        $data['server_ip'] = gethostbyname(gethostname());
+
+        #return response()->json(['quote' => $randomQuote, 'server_ip' => gethostbyname(gethostname())]);
+        return view('randomQuoteView')->with('data', $data);
+
     }
 }
